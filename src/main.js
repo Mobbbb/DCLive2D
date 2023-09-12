666d5d542d44f278056ce9aa8c5629a886a98d7c
Mobbbb <491450147@qq.com>
2023-09-12 17:37:25 +0800

    ADD ICON

--- src/main.js
+++ src/main.js
@@ -1,35 +1,57 @@
 const App = Vue.createApp({
     data() {
         return {
+            showDrawer: false,
+            activeTab: 'L',
             selectedChildId: 'c000',
             selectedVariantId: '',
             selectedChildConfig: {
                 variants: {},
             },
-            CHILDS_CODE_MAP,
-            viewer: null,
+            CHILDS_ATTR_GROUP: [],
+            
+            ATTR_MAP,
+            CODE_MAP,
         }
     },
+    computed: {
+        ACTIVE_CHILDS_CODE_LIST() {
+            return this.CHILDS_ATTR_GROUP[this.activeTab]
+        },
+    },
     mounted() {
-        this.viewer = document.getElementsByTagName('iframe')[0]
-        this.changeChilds()
+        this.CHILDS_ATTR_GROUP = {}
+        Object.keys(CHILDS_CODE_MAP).forEach(item => {
+            const attr = CHILDS_CODE_MAP[item].attribute || 'UNKNOWN'
+            if (!this.CHILDS_ATTR_GROUP[attr]) {
+                this.CHILDS_ATTR_GROUP[attr] = [CHILDS_CODE_MAP[item]]
+            } else {
+                this.CHILDS_ATTR_GROUP[attr].push(CHILDS_CODE_MAP[item])
+            }
+        })
     },
     methods: {
+        selectChilds(list) {
+            this.showDrawer = true
+            this.selectedChildConfig = list
+            this.selectedChildId = this.selectedChildConfig.id
+            this.selectedVariantId = '01'
+            this.$nextTick(() =>{
+                this.updateViews()
+            })
+        },
         // todo  + s 温泉
         changeChilds() {
-            this.selectedChildConfig = CHILDS_CODE_MAP[this.selectedChildId]
-            this.selectedVariantId = Object.keys(this.selectedChildConfig.variants)[0]
-            console.log(this.selectedChildConfig.variants)
-            console.log(this.selectedVariantId)
             this.updateViews()
         },
         changeVariants() {
             this.updateViews()
         },
         updateViews() {
+            const viewer = document.getElementsByTagName('iframe')[0]
             let size = 1000
             let scale = 1
-            this.viewer.src = `./src/views/canvas.html?code=${this.selectedChildId}_${this.selectedVariantId}&size=${size}&scale=${scale}`
+            viewer.src = `./src/views/canvas.html?code=${this.selectedChildId}_${this.selectedVariantId}&size=${size}&scale=${scale}`
         },
     },   
 })
